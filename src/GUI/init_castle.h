af7d0f8f345948937b6481573272fcb327ed27cf
Maciej <maciej.kurzawa@student.put.poznan.pl>
2020-06-05 22:17:43 +0200

    Shaders wanted

--- src/GUI/init_castle.h
+++ src/GUI/init_castle.h
@@ -9,9 +9,11 @@
 #include "modelTemplate.h"
 #include "camera.h"
 
+
 class InitCastle
 {
 public:
+
 	InitCastle();
 	void CreateObject(std::string name, glm::vec3 position, glm::vec3 scale, glm::vec3 rotate, float angle, glm::vec3 color);
 	void CreateTemplate(std::string path, std::string name, std::shared_ptr<ShaderProgram> shader);
@@ -20,7 +22,9 @@ public:
 	std::unique_ptr<Model> LoadPlayer();
 	std::vector<std::shared_ptr<ModelTemplate>> LoadTemplates(std::shared_ptr<ShaderProgram> shader);
 	std::vector<Model> LoadObjects();
+
 private:
+
 	GLfloat scale_x, scale_y, scale_z, pos_x, pos_y, pos_z;
 	std::vector<std::shared_ptr<ModelTemplate>> _modelTemplates;
 	std::vector<Model> _objects;
