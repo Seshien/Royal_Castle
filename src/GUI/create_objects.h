ad1df963b0939a59a23a10a873abdde31b8c8060
Maciej <maciej.kurzawa@student.put.poznan.pl>
2020-06-05 12:59:09 +0200

    Chyba zrobione, ale nie wiem bo nie mam obj

--- src/GUI/create_objects.h
+++ src/GUI/create_objects.h
@@ -9,17 +9,20 @@
 #include "modelTemplate.h"
 #include "camera.h"
 
-class Create_objects
+class InitCastle
 {
 public:
-	Create_objects();
+	InitCastle();
 	void CreateObject(std::string name, glm::vec3 position, glm::vec3 scale, glm::vec3 rotate, float angle, glm::vec3 color);
 	void CreateTemplate(std::string path, std::string name, std::shared_ptr<ShaderProgram> shader);
 	std::shared_ptr<ModelTemplate> FindTemplate(std::string name);
-	
-	std::vector<Model> objects;
+
+	std::unique_ptr<Model> LoadPlayer();
+	std::vector<std::shared_ptr<ModelTemplate>> LoadTemplates(std::shared_ptr<ShaderProgram> shader);
+	std::vector<Model> LoadObjects();
+private:
 	GLfloat scale_x, scale_y, scale_z, pos_x, pos_y, pos_z;
-	std::shared_ptr<ShaderProgram> TexturedShader;
-	std::vector<std::shared_ptr<ModelTemplate>> modelTemplates;
-	std::unique_ptr<Model> player_ptr;
+	std::vector<std::shared_ptr<ModelTemplate>> _modelTemplates;
+	std::vector<Model> _objects;
+	std::unique_ptr<Model> _player_ptr;
 };
